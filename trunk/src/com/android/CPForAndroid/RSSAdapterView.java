d63697635b7ba5cefd1b5c9c02c975436b46a8ac
joesox <joesox@7d7e76c5-6da2-f37b-0e5b-e5e963096962>
2010-08-24 13:38:51 +0000

    v1.7

---
+++ trunk/src/com/android/CPForAndroid/RSSAdapterView.java
@@ -0,0 +1,31 @@
+package com.android.CPForAndroid;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.CPForAndroid.R;
+
+public class RSSAdapterView extends LinearLayout {
+
+	public RSSAdapterView(Context context, RSSItem entry) {
+		super(context);
+		
+		this.setOrientation(VERTICAL);
+		this.setTag(entry);
+		
+		View v = inflate(context, R.layout.rss_row, null);
+		
+		TextView tvContact = (TextView)v.findViewById(R.id.tvTitle);
+		tvContact.setText(entry.getTitle());
+		
+		TextView tvPhone = (TextView)v.findViewById(R.id.tvAuthor);
+		tvPhone.setText(entry.getAuthor());
+		
+		TextView tvMail = (TextView)v.findViewById(R.id.tvTime);
+		tvMail.setText(entry.getPubDate());
+		
+		addView(v);
+	}
+}
