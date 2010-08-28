4e7e085a19948e41298b153ce952e83d9ca9a783
joesox <joesox@7d7e76c5-6da2-f37b-0e5b-e5e963096962>
2010-08-28 06:33:04 +0000

    Version 1.11 - Fixed issue 9 [Create About in options menu]

---
+++ trunk/src/com/android/CPForAndroid/RSSFeed.java
@@ -0,0 +1,79 @@
+//////////////////////////////////////////
+//             CPForAndroid             //
+//  http://cpforandroid.googlecode.com  //
+//     Copyright (C) 2010 JPS III       //
+//         and development team         //
+// GNU General Public License version 3 //
+//////////////////////////////////////////
+package com.android.CPForAndroid;
+
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
+import com.android.CPForAndroid.RSSItem;
+
+public class RSSFeed 
+{
+	private String _title = null;
+	private String _pubdate = null;
+	private String _author = null;
+	private int _itemcount = 0;
+	private List<RSSItem> _itemlist;
+	
+	
+	RSSFeed()
+	{
+		_itemlist = new Vector(0); 
+	}
+	int addItem(RSSItem item)
+	{
+		_itemlist.add(item);
+		_itemcount++;
+		return _itemcount;
+	}
+	int removeItem(RSSItem item)
+	{
+		_itemlist.remove(item);
+		_itemcount--;
+		return _itemcount;
+	}
+	RSSItem getItem(int location)
+	{
+		return _itemlist.get(location);
+	}
+	List<RSSItem> getAllItems()
+	{
+		return _itemlist;
+	}
+	int getItemCount()
+	{
+		return _itemcount;
+	}
+	void setTitle(String title)
+	{
+		_title = title;
+	}
+	void setPubDate(String pubdate)
+	{
+		_pubdate = pubdate;
+	}
+	void setAuthor(String author)
+	{
+		_author = author;
+	}
+	String getTitle()
+	{
+		return _title;
+	}
+	String getPubDate()
+	{
+		return _pubdate;
+	}
+	String getAuthor()
+	{
+		return _author;
+	}
+	
+	
+}
